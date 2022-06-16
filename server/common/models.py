89bac616c358304409bda1109b34fdf117074a7d
Pham Ngoc Vu <phamngcvu21@gmail.com>
2022-06-16 16:14:24 +0700

    common model

--- server/common/models.py
+++ server/common/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Common(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
