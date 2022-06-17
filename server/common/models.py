f052611acfe3659a3a72e4f343a732c16d62c992
Pham Ngoc Vu <phamngcvu21@gmail.com>
2022-06-17 10:02:26 +0700

    update common abstract model

--- server/common/models.py
+++ server/common/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Common(models.Model):
+    uuid = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         abstract = True
+        ordering = ['-created_at']
