87efa03da2feb7be608eb522e59abf99899aa173
Pham Ngoc Vu <phamngcvu21@gmail.com>
2022-06-17 10:02:49 +0700

    set up aws s3 amazon

--- server/rum4hire_server/settings.py
+++ server/rum4hire_server/settings.py
@@ -44,11 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'graphene_django',
     'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
     'graphql_auth',
     'user',
-    'common'
+    'common',
 ]
 
 MIDDLEWARE = [
@@ -181,3 +182,12 @@ GRAPHQL_AUTH = {
     'EXPIRATION_ACTIVATION_TOKEN': timedelta(days=7),
     'REGISTER_MUTATION_FIELDS': ['email', 'username'],
 }
+
+
+# aws
+
+DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
